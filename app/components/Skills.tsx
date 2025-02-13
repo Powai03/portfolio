03a074fce8db82ee97e816281257e46157002626
Kilian Delcenserie <kilian.dlcsr92@gmail.com>
2025-02-13 17:01:05 +0100

    fix margin commit

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -43,7 +43,7 @@ const Skills: React.FC = () => {
   const [hoveredSkill, setHoveredSkill] = useState<number | null>(null);
 
   return (
-    <section id="abilities"className="w-full lg:h-96 bg-[#121212] text-white py-12 flex flex-col items-center mb-32">
+    <section id="abilities" className="w-full lg:h-auto bg-[#121212] text-white py-12 flex flex-col items-center mb-32">
         <h2 className="text-transparent lg:text-6xl text-2xl font-bold lg:mb-8 mb-6 bg-clip-text bg-gradient-to-r from-blue-500 to-green-600">Mes compétences</h2>
         {
       /* Boutons de catégorie */}
